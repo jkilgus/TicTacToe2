597300268865bd1aa3a24806dddb6f580b943930
James Kilgus <jkilgus@gmail.com>
2019-04-21 18:08:44 -0400

    Game is fully functional for 2 players on the same phone. Next step is to add AI to play against

--- app/src/main/java/com/example/tictactoe2/game.java
+++ app/src/main/java/com/example/tictactoe2/game.java
@@ -1,38 +1,34 @@
 package com.example.tictactoe2;
-
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class game extends AppCompatActivity {
     private Button exit;
-    private ImageButton one;
-    private ImageButton two;
-    private ImageButton three;
-    private ImageButton four;
-    private ImageButton five;
-    private ImageButton six;
-    private ImageButton seven;
-    private ImageButton eight;
-    private ImageButton nine;
 
+    /**
+     * gamePlay is an array with 9 elements, one for each place in tictactoe
+     * 0 is no one has played there
+     * 1 is player one has played there
+     * 2 is player two have played there
+     */
+    private int[] gamePlays = {0,0,0,0,0,0,0,0,0};
 
-    // Setup game
+    /** ArrayList of the Image Buttons */
+    private View[] numbers = new View[9];
 
-    // 2 = nobody played that square yet
-    // 1 = X played
-    // 0 = O played
-    private int[] game = {2,2,2,2,2,2,2,2,2};
-    private ImageButton[] numbers = {one,two,three,four,five,six,seven,eight,nine};
+    /** If its player one or player two's turn
+     * if true then player one's turn
+     * if false then player two's turn*/
+    private boolean turn;
 
-    // If its you're turn
-    private boolean turn = true;
+    /** Number of turns that have gone by */
+    private int turnNumber;
 
-    // If the game is running
-    private boolean run = true;
+    /**If the game has a winner then true, if not, then false */
+    private boolean winner;
 
 
     @Override
@@ -40,100 +36,22 @@ public class game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-
+        turn = true;
+        turnNumber = 0;
+        winner = false;
 
         exit = findViewById(R.id.exit);
-        one = findViewById(R.id.one);
-        two = findViewById(R.id.two);
-        three = findViewById(R.id.three);
-        four = findViewById(R.id.four);
-        five = findViewById(R.id.five);
-        six = findViewById(R.id.six);
-        seven = findViewById(R.id.seven);
-        eight = findViewById(R.id.eight);
-        nine = findViewById(R.id.nine);
 
+        numbers[0] = findViewById(R.id.one);
+        numbers[1] = findViewById(R.id.two);
+        numbers[2] = findViewById(R.id.three);
+        numbers[3] = findViewById(R.id.four);
+        numbers[4] = findViewById(R.id.five);
+        numbers[5] = findViewById(R.id.six);
+        numbers[6] = findViewById(R.id.seven);
+        numbers[7] = findViewById(R.id.eight);
+        numbers[8] = findViewById(R.id.nine);
 
-        one.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                one.setBackgroundResource(R.drawable.x);
-                turn = false;
-                game[1] = 1;
-            }
-        });
-
-        two.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                two.setBackgroundResource(R.drawable.x);
-                turn = false;
-                game[2] = 1;
-            }
-        });
-
-        three.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                three.setBackgroundResource(R.drawable.x);
-                turn = false;
-                game[3] = 1;
-            }
-        });
-
-        four.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                four.setBackgroundResource(R.drawable.x);
-                turn = false;
-                game[4] = 1;
-            }
-        });
-
-        five.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                five.setBackgroundResource(R.drawable.x);
-                turn = false;
-                game[5] = 1;
-            }
-        });
-
-        six.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                six.setBackgroundResource(R.drawable.x);
-                turn = false;
-                game[6] = 1;
-            }
-        });
-
-        seven.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                seven.setBackgroundResource(R.drawable.x);
-                turn = false;
-                game[7] = 1;
-            }
-        });
-
-        eight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                eight.setBackgroundResource(R.drawable.x);
-                turn = false;
-                game[8] = 1;
-            }
-        });
-
-        nine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                nine.setBackgroundResource(R.drawable.x);
-                turn = false;
-                game[0] = 1;
-            }
-        });
 
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -142,24 +60,134 @@ public class game extends AppCompatActivity {
                 finish();
             }
         });
+
     }
 
+    public void buttonClick(View view) {
 
-        private void computerGo() {
-                while (!turn) {
-                    for (int i = 0; i <= 8; i++) {
-                        if (game[i] == 2) {
-                            game[i] = 0;
-                            numbers[i].setBackgroundResource(R.drawable.o);
-                            turn = true;
-                        }
+        TextView output = findViewById(R.id.game_state);
+
+        turnNumber++;
+
+        // Player one's turn
+        if (turnNumber == 1 || turnNumber == 3 || turnNumber == 5 || turnNumber == 7 || turnNumber == 9) {
+            if (!winner) {
+                view.setBackgroundResource(R.drawable.x);
+
+                for (int i = 0; i < 9; i++) {
+                    if (view == numbers[i]) {
+                        gamePlays[i] = 1;
                     }
                 }
 
+                view.setEnabled(false);
+                output.setText("O's turn to play!");
+                checkWin();
+            }
         }
 
+        // Player two's turn
+        if (turnNumber == 2 || turnNumber == 4 || turnNumber == 6 || turnNumber == 8) {
+            if (!winner) {
+                view.setBackgroundResource(R.drawable.o);
 
-
-
-
+                for (int i = 0; i < 9; i++) {
+                    if (view == numbers[i]) {
+                        gamePlays[i] = 2;
+                    }
+                }
+                view.setEnabled(false);
+                output.setText("X's turn to play!");
+                checkWin();
+            }
+        }
     }
+
+    private void checkWin() {
+        checkWinPlayerOne();
+
+        checkWinPlayerTwo();
+
+        checkTie();
+    }
+
+    private void checkWinPlayerOne () {
+        TextView output = findViewById(R.id.game_state);
+        if (gamePlays[0] == 1 && gamePlays[1] == 1 && gamePlays[2] == 1) {
+            output.setText("The winner is X!");
+            winner = true;
+        }
+        if(gamePlays[0] == 1 && gamePlays[3] == 1 && gamePlays[6] == 1) {
+            output.setText("The winner is X!");
+            winner = true;
+        }
+        if(gamePlays[2] == 1 && gamePlays[5] == 1 && gamePlays[8] == 1) {
+            output.setText("The winner is X!");
+            winner = true;
+        }
+        if(gamePlays[6] == 1 && gamePlays[7] == 1 && gamePlays[8] == 1) {
+            output.setText("The winner is X!");
+            winner = true;
+        }
+        if(gamePlays[3] == 1 && gamePlays[4] == 1 && gamePlays[5] == 1) {
+            output.setText("The winner is X!");
+            winner = true;
+        }
+        if(gamePlays[1] == 1 && gamePlays[4] == 1 && gamePlays[7] == 1) {
+            output.setText("The winner is X!");
+            winner = true;
+        }
+        if(gamePlays[0] == 1 && gamePlays[4] == 1 && gamePlays[8] == 1) {
+            output.setText("The winner is X!");
+            winner = true;
+        }
+        if(gamePlays[6] == 1 && gamePlays[4] == 1 && gamePlays[2] == 1) {
+            output.setText("The winner is X!");
+            winner = true;
+        }
+    }
+
+    private void checkWinPlayerTwo() {
+        TextView output = findViewById(R.id.game_state);
+        if (gamePlays[0] == 2 &&  gamePlays[1] == 2 && gamePlays[2] == 2) {
+            output.setText("The winner is 0!");
+            winner = true;
+        }
+        if(gamePlays[0] == 2 && gamePlays[3] == 2 && gamePlays[6] == 2) {
+            output.setText("The winner is O!");
+            winner = true;
+        }
+        if(gamePlays[2] == 2 && gamePlays[5] == 2 && gamePlays[8] == 2) {
+            output.setText("The winner is O!");
+            winner = true;
+        }
+        if(gamePlays[6] == 2 && gamePlays[7] == 2 && gamePlays[8] == 2) {
+            output.setText("The winner is O!");
+            winner = true;
+        }
+        if(gamePlays[3] == 2 && gamePlays[4] == 2 && gamePlays[5] == 2) {
+            output.setText("The winner is O!");
+            winner = true;
+        }
+        if(gamePlays[1] == 2 && gamePlays[4] == 2 && gamePlays[7] == 2) {
+            output.setText("The winner is O!");
+            winner = true;
+        }
+        if(gamePlays[0] == 2 && gamePlays[4] == 2 && gamePlays[8] == 2) {
+            output.setText("The winner is O!");
+            winner = true;
+        }
+        if(gamePlays[6] == 2 && gamePlays[4] == 2 && gamePlays[2] == 2) {
+            output.setText("The winner is O!");
+            winner = true;
+        }
+    }
+
+    private void checkTie() {
+        TextView output = findViewById(R.id.game_state);
+        if(gamePlays[0] != 0 && gamePlays[1] != 0 && gamePlays[2] != 0 && gamePlays[3] != 0 && gamePlays[4] != 0
+                && gamePlays[5] != 0 && gamePlays[6] != 0 && gamePlays[7] != 0 && gamePlays[8] != 0 && !winner) {
+            output.setText("Tie game!");
+        }
+    }
+}
